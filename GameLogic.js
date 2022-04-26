b06cfbcc5e6112102e834bc57f52d05ac98df836
Luke Jennings <lukejennings123@gmail.com>
2022-04-26 23:10:59 +0100

    completed loop to add as many rows/columns as we want + added grid via css

--- GameLogic.js
+++ GameLogic.js
@@ -1,25 +1,16 @@
 const container = document.querySelector('.container');
 
-//I need to create a function that creates rows and columns. This will involve a loop 
-// loops around x number of times (depending on input number) and creates the grid. 
 
-function row(num){
+function row(num, columns){
 for (let i = 0; i < num; i++){
     const div = document.createElement('div');
     container.appendChild(div);
+
+for (let i = 0; i < columns; i++){
+    const div2 = document.createElement('div2');
+    div.appendChild(div2);
 }
 }
-/* the above function has created the 16 rows but I now need to create the columns function. 
-I belive this means we have to turn each row element at the top into another container div
-so that we can populate the columns but im not sure */ 
-
-
-// function column(num){
-//     for (let i = 0; i < num; i++){
-//         const div = document.createElement('div');
-//         container.appendChild(div);
-//     }
-//     }
-    // column(16);
-    row(16);
+}
+    row(16, 16);
     
